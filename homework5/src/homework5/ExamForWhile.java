6db4cd682d9dff992d41ea5a434522b063ff547a
legungu <legungu@naver.com>
2016-10-07 16:14:30 +0900

    ok

---
+++ homework5/src/homework5/ExamForWhile.java
@@ -0,0 +1,34 @@
+package homework5;
+
+
+import java.util.*;
+public class ExamForWhile {
+	public void main (String[] args){
+		
+		Scanner s = new Scanner(System.in);
+		int i= s.nextInt();
+		if (i==1)
+		{
+			new input().SumOfNumbers();
+		}
+		else if (i==2)
+		{
+			new input().MaxAndMin();
+		}
+		else if (i==3)
+		{
+			new input().SumAndAvg();
+		}
+		else if (i==4)
+		{
+			new input().GuGuDan();
+		}
+		else if (i==5)
+		{
+			new input().GuGuDan2();
+		}
+		
+	}
+	
+	
+}
